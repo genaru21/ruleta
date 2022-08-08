5d15ab4a6b909eb3eafeebb3be5382cc899c440c
genaru21 <106562056+genaru21@users.noreply.github.com>
2022-08-08 15:35:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -41,29 +41,35 @@ function premio(premios){
 
   setTimeout(() => {
   switch (true) {
-    case valor > 0 && valor <= 45:
+    case valor > 0 && valor <= 36:
      window.alert('I want to work with you, apple');
      break;
-     case valor > 45 && valor <= 90:
+     case valor > 36 && valor <= 72:
      window.alert('My product is not engraved');
      break;
-     case valor > 90 && valor <= 135:
+     case valor > 72 && valor <= 108:
      window.alert('Installments');
      break; 
-     case valor > 135 && valor <= 180:
+     case valor > 108 && valor <= 144:
      window.alert('Replace screen protector');
      break;
-     case valor > 180 && valor <= 225:
+     case valor > 144 && valor <= 180:
      window.alert('Change my account');
      break; 
-     case valor > 225 && valor <= 270:
+     case valor > 180 && valor <= 216:
      window.alert('Aide au partage familial');
      break;
-     case valor > 270 && valor <= 315:
+     case valor > 216 && valor <= 252:
      window.alert('EPP, please');
      break;
-     case valor > 315 && valor <= 360:
+     case valor > 252 && valor <= 288:
      window.alert('I want to know your sales');
+     break;
+     case valor > 288 && valor <= 324:
+     window.alert('Repérer ma souris');
+     break;
+     case valor > 324 && valor <= 360:
+     window.alert('Mon écran est cassé, j´ai besoin d´aide, est endommagé');
      break;
   }
 
